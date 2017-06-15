b01ddc9697c8dbf047d7cc12a80928bbda66a9b6
Mats Gemmeke <matsgemmeke@gmail.com>
2017-06-15 12:36:08 +0200

    Login scherm layout toegevoegd

---
+++ app/src/main/java/nl/avans/film_app/Config.java
@@ -0,0 +1,7 @@
+package nl.avans.film_app;
+
+public class Config {
+
+    public static final String DATABASE_ADDRESS = "146.185.130.82";
+    public static final String SERVER_ADDRESS = null;
+}
--- app/src/main/java/nl/avans/film_app/MainActivity.java
+++ app/src/main/java/nl/avans/film_app/MainActivity.java
@@ -2,12 +2,30 @@ package nl.avans.film_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText password, username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.button = (Button) findViewById(R.id.activity_login_button);
+        this.password = (EditText) findViewById(R.id.activity_login_password);
+        this.username = (EditText) findViewById(R.id.activity_login_username);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Login", username.getText().toString() + " " + password.getText().toString());
+            }
+        });
     }
 }
