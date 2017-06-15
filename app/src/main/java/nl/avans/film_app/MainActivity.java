443a3054cf405bd390b000c8f61d6705b1bd64fa
Mats Gemmeke <matsgemmeke@gmail.com>
2017-06-15 14:02:08 +0200

    Registreren en inloggen werkt

--- app/src/main/java/nl/avans/film_app/MainActivity.java
+++ app/src/main/java/nl/avans/film_app/MainActivity.java
@@ -1,16 +1,33 @@
 package nl.avans.film_app;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
     private EditText password, username;
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +36,70 @@ public class MainActivity extends AppCompatActivity {
 
         this.button = (Button) findViewById(R.id.activity_login_button);
         this.password = (EditText) findViewById(R.id.activity_login_password);
+        this.register = (TextView) findViewById(R.id.activity_login_register);
         this.username = (EditText) findViewById(R.id.activity_login_username);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.i("Login", username.getText().toString() + " " + password.getText().toString());
+                String body = "{ \"username\": \"" + username.getText().toString() + "\", \"password\": \"" + password.getText().toString() + "\" }";
+                Log.i("Login", body);
+
+                try {
+                    JSONObject jsonBody = new JSONObject(body);
+                    JsonObjectRequest jsObjRequest = new JsonObjectRequest(
+                            Request.Method.POST,
+                            Config.SERVER_ADDRESS + ":3000/api/v1/login",
+                            jsonBody,
+                            new com.android.volley.Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    // Succesvol response - dat betekent dat we een geldig token hebben.
+
+                                    // We hebben nu het token. We kiezen er hier voor om
+                                    // het token in SharedPreferences op te slaan. Op die manier
+                                    // is het token tussen app-stop en -herstart beschikbaar -
+                                    // totdat het token expired.
+                                    try {
+                                        String token = response.getString("token");
+
+                                        Context context = getApplicationContext();
+                                        SharedPreferences sharedPref = context.getSharedPreferences(
+                                                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = sharedPref.edit();
+                                        editor.putString(getString(R.string.saved_token), token);
+                                        editor.commit();
+                                        Log.i("Login", "Token = " + token);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new com.android.volley.Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    error.printStackTrace();
+                                }
+                            }) {
+                        public Map<String, String> getHeaders() throws AuthFailureError {
+                            Map<String, String> headers = new HashMap<>();
+                            headers.put("Content-Type", "application/json; charset=utf-8");
+                            return headers;
+                        }
+                    };
+
+                    jsObjRequest.setRetryPolicy(new DefaultRetryPolicy(1500, 2, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+                    VolleyRequestQueue.getInstance(MainActivity.this).addToRequestQueue(jsObjRequest);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
             }
         });
     }
---
+++ app/src/main/java/nl/avans/film_app/RegisterActivity.java
@@ -0,0 +1,77 @@
+package nl.avans.film_app;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText email, password, username;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        this.button = (Button) findViewById(R.id.activity_register_button);
+        this.email = (EditText) findViewById(R.id.activity_register_email);
+        this.password = (EditText) findViewById(R.id.activity_register_password);
+        this.username = (EditText) findViewById(R.id.activity_register_username);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String body = "{ \"first_name\": \"" + username.getText().toString() + "\", \"last_name\": \"" + password.getText().toString() + "\", \"email\": \"" + email.getText().toString() + "\" }";
+                Log.i("Login", body);
+
+                try {
+                    JSONObject jsonBody = new JSONObject(body);
+                    JsonObjectRequest jsObjRequest = new JsonObjectRequest(
+                            Request.Method.POST,
+                            Config.SERVER_ADDRESS + ":3000/api/v1/register",
+                            jsonBody,
+                            new com.android.volley.Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    finish();
+                                }
+                            }, new com.android.volley.Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            error.printStackTrace();
+                        }
+                    }) {
+                        public Map<String, String> getHeaders() throws AuthFailureError {
+                            Map<String, String> headers = new HashMap<>();
+                            headers.put("Content-Type", "application/json; charset=utf-8");
+                            return headers;
+                        }
+                    };
+
+                    jsObjRequest.setRetryPolicy(new DefaultRetryPolicy(1500, 2, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+                    VolleyRequestQueue.getInstance(RegisterActivity.this).addToRequestQueue(jsObjRequest);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
